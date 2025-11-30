417d22ce03633de7e0a9abb5fb61ad4d440a3906
Anthony-Huggins <anthonyhugginsdev@gmail.com>
2025-11-30 02:26:57 -0500

    first commit

---
+++ src/main/java/com/skillstorm/pokemonstore/PokemonstoreApplication.java
@@ -0,0 +1,13 @@
+package com.skillstorm.pokemonstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skillstorm/pokemonstore/PokemonstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillstorm.pokemonstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
