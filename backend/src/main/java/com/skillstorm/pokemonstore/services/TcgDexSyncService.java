e27c1d7b97ad726ffef726c3ec1df46677710515
Anthony Huggins <anthonyhugginsdev@gmail.com>
2025-11-30 15:45:44 -0500

    package naming fix

--- backend/src/main/java/com/skillstorm/pokemonstore/services/TcgDexSyncService.java
+++ backend/src/main/java/com/skillstorm/pokemonstore/services/TcgDexSyncService.java
@@ -11,7 +11,6 @@ import org.springframework.web.client.RestClient;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class TcgDexSyncService {
