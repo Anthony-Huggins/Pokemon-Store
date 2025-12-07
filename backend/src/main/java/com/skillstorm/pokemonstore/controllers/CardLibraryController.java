33acc1851e77d27d0e34d9c9eafccdb05585a576
Anthony Huggins <anthonyhugginsdev@gmail.com>
2025-12-07 15:17:12 -0500

    change get all cards to not get ones without image and move sorting logic to service

--- backend/src/main/java/com/skillstorm/pokemonstore/controllers/CardLibraryController.java
+++ backend/src/main/java/com/skillstorm/pokemonstore/controllers/CardLibraryController.java
@@ -3,9 +3,6 @@ package com.skillstorm.pokemonstore.controllers;
 import com.skillstorm.pokemonstore.models.CardDefinition;
 import com.skillstorm.pokemonstore.services.CardLibraryService;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
