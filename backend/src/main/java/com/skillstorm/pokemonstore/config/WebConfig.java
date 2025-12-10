182ad91657f98897a1a24cc95045420cc840e14c
Anthony Huggins <anthonyhugginsdev@gmail.com>
2025-12-10 18:42:38 -0500

    added WebConfig to allow frontend to request png file from backend efficiently

---
+++ backend/src/main/java/com/skillstorm/pokemonstore/config/WebConfig.java
@@ -0,0 +1,25 @@
+package com.skillstorm.pokemonstore.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // 1. Define the physical path to the folder
+        // Paths.get("card_images") looks for the folder at the Project Root
+        Path uploadDir = Paths.get("card_images");
+        String uploadPath = uploadDir.toFile().getAbsolutePath();
+
+        // 2. Map the URL "/images/**" to that physical folder
+        // "file:" prefix is crucial—it tells Spring to look on the File System, not inside the JAR
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("file:" + uploadPath + "/");
+    }
+}
